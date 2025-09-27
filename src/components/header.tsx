ffaef0abd9d715e0adff31feb42cc1cb10b1c060
EliaShukrani <eliamuyizere36@gmail.com>
2025-09-27 07:29:34 +0200

    Restraunt images

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -301,7 +301,7 @@ export function Header() {
 
   return (
     <>
-      <header className="fixed top-0 left-0 right-0 z-50">
+      <header className="fixed top-0 left-0 right-0 z-50 ">
         <div className="bg-green-700 border-b border-green-600 shadow-lg">
           <div className="container mx-auto px-4 sm:px-6">
             <div className="flex items-center justify-between h-12">
